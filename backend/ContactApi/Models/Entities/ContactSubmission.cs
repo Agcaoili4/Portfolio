d712b49f7e294b2f741f21f1d8727a939b85c2a5
Jansen Agcaoili <ajagcaoili4@gmail.com>
2026-05-29 16:57:13 -0600

    Edited appsettings and added ContactSubmissions
    - Removed unwanted block of code for appSettings
    
    - Added ContactSubmission for creating the template

---
+++ backend/ContactApi/Models/Entities/ContactSubmission.cs
@@ -0,0 +1,9 @@
+
+namespace ContactApi.Models.Entities;
+
+// The class definition for ContactSubmission 
+public class ContactSubmission
+{
+    // Guid property to uniquely identify each contact submission
+    public Guid Id { get; set; }
+}
